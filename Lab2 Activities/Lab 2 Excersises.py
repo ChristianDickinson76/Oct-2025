08c189fe6204ca344c5f673e3d43ca06e4731261
ChristianDickinson76 <christiandickinson76@gmail.com>
2025-10-06 15:33:56 +0100

    Got up to task 6

--- Lab2 Activities/Lab 2 Excersises.py
+++ Lab2 Activities/Lab 2 Excersises.py
@@ -1,5 +1,6 @@
 _userInput = input("What excerise to run?")
 match _userInput:
+    
     case "1":
         itemPrice = float(input("Enter the item price: ")) 
         itemTax = itemPrice * 0.2
@@ -9,6 +10,7 @@ match _userInput:
         _quantityToPurchase = int(input("How many items do you wish to buy?")) #Takes input for how many to purchase and casts to int
         _priceForProducts = totalPrice * _quantityToPurchase
         print(f"The price for the items is {round(_priceForProducts,2)}") #Limits total cost to 2dp (For currency) and prints
+    
     case "2":
         year = int(input("Enter the year you were born: "))
         if year % 4 == 0:
@@ -22,3 +24,33 @@ match _userInput:
             print("You are older than me!")
         else:
             print("You were born in the same year as me!")
+    
+    case "3":
+        half = 1.0/2.0
+        print(type(half))
+        print(f"One half (1/2) = {half}")
+    
+    case "4":
+        _userInput = int(input("What is the temperature in C? "))
+        _tempInF = _userInput * (9/5) + 32
+        print(f"{_userInput}C in F is {_tempInF}F")
+    
+    case "5":
+        _conversionRate = float(input("£1 in $ is: "))
+        _userInput = input("Which do you wish to do?\n1 ~ Convert £ to $\n2 ~ Convert $ to £")
+        match _userInput:
+            case "1":
+                _userInput = float(input("Enter the amount in £ to convert"))
+                _convertedSum = _userInput * _conversionRate
+                print(f"The converted value of £{_userInput} is ${round(_convertedSum,2)}")
+            case "2":
+                _userInput = float(input("Enter the amount in $ to convert"))
+                _convertedSum = _userInput / _conversionRate
+                print(f"The converted value of ${_userInput} is £{round(_convertedSum,2)}")
+
+    case "6":
+        _userInput = input("Please enter 5 numbers, seperated by ,")
+        _listNumbers = _userInput.split(",")
+        _listNumbers.sort()
+        _listNumbers.reverse()
+        print(f"The largest number entered was {_listNumbers[0]}")
