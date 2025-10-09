1e094fcd91053a32df54e45d92521faa3890a670
ChristianDickinson76 <christiandickinson76@gmail.com>
2025-10-09 14:02:00 +0100

    Lab2_091025 Commit

---
+++ Lab2 Activities/Lab2_091025.py
@@ -0,0 +1,24 @@
+_userInput = input("What excersise do you want to run?: ")
+match _userInput:
+    case "1":
+        _userInput = input("Do you have a ticket? Y/N: ").capitalize()
+        if(_userInput == "Y"):
+            print("You may enter")
+        else:
+            print("You cannot enter")
+    case "2":
+        _userAge = int(input("What is your age?: "))
+        _userID = input("Do you have ID? Y/N: ").capitalize()
+        if(_userInput == "Y" and _userAge >= 18):
+            print("You may enter")
+        else:
+            print("You cannot enter")
+    case "3":
+        _userInput = input("What is the weather like?\n1 ~ Sunny\n2 ~ Rainy\n3 ~ Other\n ").capitalize()
+        if(_userInput == "1"):
+            print("Go outside")
+        elif (_userInput == "2"):
+            print("Bring an Umbrella")
+        else:
+            print("Do not go out")
+            
